e98a5c2ade9c511f9e7385d293e50115e85d7bce
Karen Irene Cano <karenirene.cano1203@gmail.com>
2018-02-20 09:41:43 +0000

    added default value

--- app.py
+++ app.py
@@ -10,12 +10,47 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-chatbot = ChatBot('Charlie',trainer='chatterbot.trainers.ListTrainer')
+
+chatbot = ChatBot(
+    'Default Response Example Bot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'I am sorry, but I do not understand.'
+        }
+    ],
+    trainer='chatterbot.trainers.ListTrainer'
+)
+
+# Train the chat bot with a few responses
+chatbot.train([
+    'How can I help you?',
+    'I want to create a chat bot',
+    'Have you read the documentation?',
+    'No, I have not',
+    'This should help get you started: http://chatterbot.rtfd.org/en/latest/quickstart.html',
+    'Who created you?',
+    'Mainly fron gunthercox but I am from Commude Philippines Inc! :)',
+    'Sino gwapo?',
+    'Sino pa? edi si Rui! ;)',
+    'Sino mga gwapo?',
+    'Required lahat gwapo at maganda sa Commude! :)'
+])
+
+
+
+
+'''chatbot = ChatBot('Charlie',trainer='chatterbot.trainers.ListTrainer')
 chatbot.train([
         "Hi, can I help you?",
         "Sure, I'd like to book a flight to Iceland.",
         "Your flight has been booked."
-    ])
+    ])'''
 #response = chatbot.get_response('I would like to book a flight.')
 
 # prepare your endpoints
